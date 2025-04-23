73871f6f573dd88bfbba8748dc2f17eddf3af403
Đạt Bùi Minh Tuấn <buiminhtuandat@gmail.com>
2025-04-23 18:43:43 +0700

    Update Infor

--- Infor/App.js
+++ Infor/App.js
@@ -10,19 +10,28 @@ export default function App() {
       </View>
       <Image
         source={require('./assets/image.jpg')}
-        style={{ width: 100, height: 100, margin: 20 }} 
-      />
+        style={styles.image}
+        />
       <View > 
-        <Text style={styles.content}>Tên: <Text style={styles.text}>Bùi Minh Tuấn Đạt</Text></Text>
+        <Text style={styles.id}>@buiminhtuandat</Text>
       </View>
-      <View > 
-        <Text style={styles.content}>Quê quán: <Text style={styles.text}>Thái Thụy, Thái Bình</Text></Text>
-      </View>
-      <View > 
-        <Text style={styles.content}>Trường: <Text style={styles.text}>Đại học FPT Hà Nội</Text></Text>
-      </View>
-      <View > 
-        <Text style={styles.content}>Sở thích: <Text style={styles.text}>Chơi game, chế tạo</Text></Text>
+      <View  style={styles.content}>
+        <View > 
+          <Text style={styles.label}>Tên:</Text>
+          <Text style={styles.text}>Bùi Minh Tuấn Đạt</Text>
+        </View>
+        <View > 
+          <Text style={styles.label}>Quê quán:</Text>
+          <Text style={styles.text}>Thái Thụy, Thái Bình</Text>
+        </View>
+        <View > 
+          <Text style={styles.label}>Trường:</Text>
+          <Text style={styles.text}>Đại học FPT Hà Nội</Text>
+        </View>
+        <View > 
+          <Text style={styles.label}>Sở thích:</Text>
+          <Text style={styles.text}>Chơi game, chế tạo</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -32,30 +41,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f4ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   content: {
     width: 300,
-    height: 40,
-    marginTop: 16,
-    paddingVertical: 8,
-    paddingHorizontal: 4,
+    height: 250,
+    marginTop: 30,
+    paddingHorizontal: 40,
     borderWidth: 2,
     borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: '#61dafb',
-    fontSize: 20,
-    fontWeight: 'bold',
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 8,
+  },
+  label: {
+    marginTop: 10,
+    color: '#888',
+    fontSize: 14,
+    marginBottom: 2,
+    textTransform: 'uppercase',
+    letterSpacing: 1,
   },
   text: {
-    fontSize: 20,
-    fontWeight: 'normal',
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#333',
+    textAlign: 'center',
   },
   header: {
     fontSize: 30,
-    color: 'green',
+    color: '#496580',
     fontWeight: 'bold',
-  }
+  },
+  image: {
+    width: 120,
+    height: 120,
+    marginTop: 20,
+    borderRadius: 120 / 2,
+    borderWidth: 3,
+    borderColor: '#2460a7',
+  },
+  id: {
+    color: '#888',
+    fontSize: 14,
+    letterSpacing: 1,
+  },
 });
